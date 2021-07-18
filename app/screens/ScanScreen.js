a156c8cfb7c43080822f64a073067b50b841eb29
J <j@Js-MacBook-Pro.local>
2021-07-18 00:38:54 -0700

    added scanner back

--- app/screens/ScanScreen.js
+++ app/screens/ScanScreen.js
@@ -51,7 +51,16 @@ const ScanScreen = ({ navigation, route }) => {
       </View>
 
       <View style={styles.body}>
-     
+        <CameraScreen
+          hideControls={false} // (default false) optional, hides camera controls
+          showCapturedImageCount={false} // (default false) optional, show count for photos taken during that capture session
+          // Barcode props
+          scanBarcode={true}
+          onReadCode={(event) => Alert.alert('QR code found')} // optional
+          showFrame={true} // (default false) optional, show frame with transparent layer (qr code or barcode will be read on this area ONLY), start animation for scanner,that stoped when find any code. Frame always at center of the screen
+          laserColor='red' // (default red) optional, color of laser in scanner frame
+          frameColor='white' // (default white) optional, color of border of scanner frame
+        />
       </View>
     </SafeAreaView> 
   );
