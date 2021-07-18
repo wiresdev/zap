706496eb9c786024405320cc5b6b7389f0461ac7
Austin Wade <me@austinhunt.co>
2021-07-17 23:18:13 -0700

    fix android camera permissions for debug target

--- app/screens/ScanScreen.js
+++ app/screens/ScanScreen.js
@@ -51,18 +51,7 @@ const ScanScreen = ({ navigation, route }) => {
       </View>
 
       <View style={styles.body}>
-      <CameraScreen
-        //actions={{ rightButtonText: 'Done', leftButtonText: 'Cancel' }}
-        //onBottomButtonPressed={(event) => this.onBottomButtonPressed(event)}
-        //hideControls={false} // (default false) optional, hides camera controls
-        showCapturedImageCount={true} // (default false) optional, show count for photos taken during that capture session
-        // Barcode props
-        scanBarcode={true}
-        onReadCode={(event) => Alert.alert('QR code found')} // optional
-        showFrame={true} // (default false) optional, show frame with transparent layer (qr code or barcode will be read on this area ONLY), start animation for scanner,that stoped when find any code. Frame always at center of the screen
-        laserColor='red' // (default red) optional, color of laser in scanner frame
-        frameColor='white' // (default white) optional, color of border of scanner frame
-      />
+     
       </View>
     </SafeAreaView> 
   );
